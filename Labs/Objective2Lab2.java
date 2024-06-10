b249122503e28498d9fd4bb76cd29fb10699cb0d
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-10 17:41:51 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,17 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Brian Brown ";
+    int age =30 ;
+    String state = "North Carolina";
+
+
+
+      System.out.print("Name: ");
+      System.out.print( name );
+      System.out.print("Age: ");
+      System.out.print( age );
+      System.out.print(" State:  ");
+      System.out.println( state );
+    
+  }
+}
