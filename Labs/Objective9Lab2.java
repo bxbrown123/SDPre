826b6a7fd928240149e6b6a646d64382012c1914
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-12 08:40:40 -0600

    complete objective9lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,20 @@
+public class Objective9Lab2 {
+  public static void main(String[] args) {
+
+    //Do NOT name them things like "method1", "method2"etc...
+
+    // Top
+    System.out.println("In main");
+
+    // Mid Must keep this for the bottom portion "in mthod" to be printed. 
+    printMessage();
+    
+    // Bot
+    System.out.println("In main");
+  }
+    // This is actually the mid.
+  public static void printMessage() {
+    System.out.println("In method");
+  }
+  
+}
