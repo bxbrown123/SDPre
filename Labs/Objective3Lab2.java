e2778cfc7dbb9d832109cd3679328342820944cd
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-11 21:13:07 -0600

    complete objective3lab2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -5,5 +5,8 @@ public class Objective3Lab2 {
 			side2 = 8.0;
       		hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));
       		System.out.println("The hypotenuse of a triangle with sides " + side1 + " and " + side2 + " is " + hypotenuse);
+      		// The a side1, the b side2 and c being hypotenuse. 
+      		// The excat text: The hypotenuse of a triangle with sides , and, and is. 
+      		// While numerical side1, side 2, hypotenuse.
 		}
 }
