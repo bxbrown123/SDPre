73e9ada4f9b1ecfb1b3dddafdf9e3bd81d07f9e7
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-12 08:24:35 -0600

    complete objective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,11 @@
+public class Objective9Lab1 {
+  public static void main(String[] args) {
+    // Method now.
+    printName();
+  }
+    // As you see here we are now using two sets.
+  public static void printName() {
+    // We are printing out here.
+    System.out.println("Hello Doug");
+  }
+}
