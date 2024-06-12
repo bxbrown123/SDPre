f89658773134c03761d927f775f52eb10eee80cc
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-12 06:33:46 -0600

    complete objective7lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,12 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+      // for counters + is going up - is going down. this is 21 numbers. 0-20
+    int counter = 0;
+
+    while(counter <= 20) {
+    
+    System.out.println(counter);
+    counter = counter + 1;
+    }
+  }
+}
