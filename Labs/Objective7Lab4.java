09e9a7f6af9ab472b920cbe54a147b8fd1c854a7
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-12 07:03:33 -0600

    complete objective7lab4

--- Labs/Objective7Lab4.java
+++ Labs/Objective7Lab4.java
@@ -1,6 +1,9 @@
 public class Objective7Lab4 {
 	public static void main(String [] args) {
-		
+		// once again 20 times
+		// every iteration will have will have the previous added to the next.
+		// ex 1+2+3
+		// the variable will be constantly updated replacing the last.
 		int count = 1;
 		
 		int sum = 0;
