48473f3f87db1229cdf627622c6dcdb909aadb2f
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-11 21:33:32 -0600

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,33 @@
+public class Objective3Lab3 {
+      public static void main(String[] args) {
+        int first;
+        int second;
+        int third;
+        int sum;
+
+        first = 1 + (int)(Math.random() * 6);        
+        second = 1 + (int)(Math.random() * 6);
+        third = 1 + (int)(Math.random() * 6);
+        sum = first + second + third;
+
+        System.out.println(first + " + " + second + " + " + third + " = " + sum);
+
+        // Expected Output x + x + x = x
+        // Each output ran will be different due to the random nature.
+        // Excat text "+ and =" and Mathematical Symbols + and 
+
+
+
+
+       
+
+       }
+}  
+
+
+
+
+
+
+
+
