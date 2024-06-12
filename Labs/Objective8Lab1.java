0a631e2f1be955717315849def5b76e1527bb38a
brianxzaviorbrown <brianxzaviorbrown@yahoo.com>
2024-06-12 07:48:34 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,14 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    
+    // a new range from 1-10-1 up and down.
+    // the up 1-10
+    for (int increaseCounter = 1; increaseCounter <= 10; increaseCounter ++) {
+      System.out.println(increaseCounter);
+    }
+    // the down 10-1
+    for (int decreaseCounter = 10; decreaseCounter >= 1; decreaseCounter --) {
+      System.out.println(decreaseCounter);
+    }
+  }
+}
